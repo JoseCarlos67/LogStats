848fed22068aa8ea136b37664983ac35f112c84c
JoseCarlos67 <josecarlos.silesteves@gmail.com>
2026-02-07 22:32:01 -0300

    feat: estrutura inicial do projeto com Maven, JUnit5 e Lombok

---
+++ src/main/java/com/JCarlos67/logstats/App.java
@@ -0,0 +1,13 @@
+package com.JCarlos67.logstats;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
